9eead53d0968bc9277f151499723cc63eba31ccf
M-Hamza380 <hamza.anjum380@gmail.com>
2025-09-30 22:37:04 +0500

    working on servers

--- project_template.py
+++ project_template.py
@@ -10,7 +10,6 @@ logging.basicConfig(
 
 list_of_files = [
     f"src/backend/src/__ini__.py",
-    f"src/backend/src/main.py",
     f"src/backend/src/contants/__init__.py",
     f"src/backend/src/utilities/__init__.py",
     f"src/backend/src/utilities/logger.py",
@@ -25,24 +24,30 @@ list_of_files = [
     f"src/backend/src/routers/challenge.py",
     f"src/backend/src/routers/webhooks.py",
     f"src/backend/server.py",
+    f"src/backend/__init__.py",
     f"src/backend/.env",
     f"src/backend/.env-example",
     f"src/backend/.dockerignore",
     f"src/backend/Dockerfile",
+    "nginx/nginx.conf",
+    "docker-compose.yaml",
 ]
 
 
 for file in list_of_files:
-    filepath = Path(file)
-    file_dir, file_name = os.path.split(filepath)
+    if file:
+        filepath = Path(file)
+        file_dir, file_name = os.path.split(filepath)
 
-    if filepath != "":
-        os.makedirs(file_dir, exist_ok=True)
-        logging.info(f"Creating directory: {file_dir} for file: {file_name}!")
-    
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(file, 'w') as f:
-            pass
-            logging.info(f"Creating empty file: {file}!")
+        if file_dir != "":
+            os.makedirs(file_dir, exist_ok=True)
+            logging.info(f"Creating directory: {file_dir} for file: {file_name}!")
+        
+        if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+            with open(file, 'w') as f:
+                pass
+                logging.info(f"Creating empty file: {file}!")
+        else:
+            logging.info(f"That {file} already exists!")
     else:
-        logging.info(f"That {file} already exists!")
+        logging.info(f"File: {file} is empty!")
